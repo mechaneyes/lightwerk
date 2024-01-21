024d6c3cc0c59fec4839e6d518b67724cc20e12f
mechaneyes <ray@mechaneyes.com>
2024-01-21 06:39:56 -0500

    commented out upload page

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,44 +1,44 @@
-"use client";
+// "use client";
 
-// import * as cloudinary from "cloudinary";
-// import UploadWidget from "@/components/UploadWidget";
-import { CldUploadWidget } from "next-cloudinary";
+// // import * as cloudinary from "cloudinary";
+// // import UploadWidget from "@/components/UploadWidget";
+// import { CldUploadWidget } from "next-cloudinary";
 
-const ImageUpload = () => {
-  const handleUpload = (result) => {
-    if (result.info.secure_url) {
-      // Successful upload
-      console.log("Upload successful. Secure URL:", result.info.secure_url);
-    } else {
-      // Handle the error
-      console.error("Upload failed. Error message:", result.info.error.message);
-    }
-  };
+// const ImageUpload = () => {
+//   const handleUpload = (result) => {
+//     if (result.info.secure_url) {
+//       // Successful upload
+//       console.log("Upload successful. Secure URL:", result.info.secure_url);
+//     } else {
+//       // Handle the error
+//       console.error("Upload failed. Error message:", result.info.error.message);
+//     }
+//   };
 
-  return (
-    <div>
-      <CldUploadWidget
-        options={{
-          name: "dhbk213bt",
-          folder: "nucleus",
-          singleUploadAutoClose: true,
-          useFilename: true,
-          sources: ["local"],
-        }}
-        signatureEndpoint="/api/sign-image"
-        uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME_LOCAL}
-        onUpload={handleUpload}
-      >
-        {({ open }) => {
-          function handleOnClick(e) {
-            e.preventDefault();
-            open();
-          }
-          return <button onClick={handleOnClick}>Upload an Image</button>;
-        }}
-      </CldUploadWidget>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <CldUploadWidget
+//         options={{
+//           name: "dhbk213bt",
+//           folder: "nucleus",
+//           singleUploadAutoClose: true,
+//           useFilename: true,
+//           sources: ["local"],
+//         }}
+//         signatureEndpoint="/api/sign-image"
+//         uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME_LOCAL}
+//         onUpload={handleUpload}
+//       >
+//         {({ open }) => {
+//           function handleOnClick(e) {
+//             e.preventDefault();
+//             open();
+//           }
+//           return <button onClick={handleOnClick}>Upload an Image</button>;
+//         }}
+//       </CldUploadWidget>
+//     </div>
+//   );
+// };
 
-export default ImageUpload;
+// export default ImageUpload;
