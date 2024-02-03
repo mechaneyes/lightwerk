03db9ffc7a54f70be8c483fb0c55b9c0a7f901ad
mechaneyes <ray@mechaneyes.com>
2024-02-03 12:04:41 -0500

    updated carousel settings
    ° editing photo filename string . cleaned up
    ° dropped dots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,27 @@ import Carousel from "@/components/Carousel";
 import type { PhotoType } from "@/utils/types";
 
 async function fetchData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/photo?cloudinary_folder=recognition`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}api/photo?cloudinary_folder=recognition`
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
-  } 
+  }
 
   return res.json();
 }
 
 // https://medium.com/@moh.mir36/open-graph-with-next-js-v13-app-directory-22c0049e2087
-// 
+//
 export const metadata: Metadata = {
-  metadataBase: new URL('https://lightwerk.digital/'),
+  metadataBase: new URL("https://lightwerk.digital/"),
   title: "Shadowing",
   description: "We are the Dreamers of the Dream",
   openGraph: {
     images: [
       {
-        url: 'https://res.cloudinary.com/dhbk213bt/image/upload/v1706917124/lightwerk/og/og-shadowing.jpg',
+        url: "https://res.cloudinary.com/dhbk213bt/image/upload/v1706917124/lightwerk/og/og-shadowing.jpg",
       },
     ],
   },
@@ -30,7 +32,7 @@ export const metadata: Metadata = {
 
 const Nucleus: NextPage<{}> = async ({}) => {
   const carouselSettings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -48,7 +50,6 @@ const Nucleus: NextPage<{}> = async ({}) => {
           {fetchedPhotos.map((photo: PhotoType, index: number) => (
             <div key={index} className="slide-holder">
               <Image
-                // src={photo?.url.replace(".png", ".gif") ?? ""}
                 src={photo?.url}
                 width={1280}
                 height={1280}
@@ -56,7 +57,7 @@ const Nucleus: NextPage<{}> = async ({}) => {
                 alt={`Hexagram Facial Recognition: ${photo?.filename}`}
                 priority={true}
               />
-              <h3>{photo?.filename}</h3>
+              <h3>{photo?.filename?.split("_").slice(0, 2).join("_")}</h3>{" "}
             </div>
           ))}
         </Carousel>
